957687742db26c8f8aee4c3ba2edfbc2a3fe3257
t-krish <95111595+t-krish@users.noreply.github.com>
2021-11-26 21:00:05 -0500

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,17 @@
+
+x = input('How many numbers should the pyramid be?: ')
+x = int(x)
+x = x + 1
+y = ' ' * x
+s = 0
+line = 0
+for i in range(1, x) :
+    line = line + 1
+    st1 =''
+    for k in range(i,i+line):
+        s = s+1
+        st1 = str(st1) + ' ' + str(s)
+    print(y, st1, y)
+    y = ' ' * (x-i)
+
+#complete
